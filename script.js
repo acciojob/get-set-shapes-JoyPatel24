eaec3347c827878d025f1f0fabac8a199e135560
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-07-26 14:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate and return the area of the rectangle
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+    // Call the super constructor of Rectangle with side as both width and height
+    super(side, side);
+  }
+
+  // Method to calculate and return the perimeter of the square
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
